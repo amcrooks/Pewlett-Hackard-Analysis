4cf2fd0f357b36f4fc773883c8d213335b41c7e0
Alexis Crooks <lexiec@Alexiss-MBP.lan>
2021-12-12 02:49:49 -0600

    challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -13,3 +13,20 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT desc;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+From employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
